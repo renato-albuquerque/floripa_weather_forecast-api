1aa2e13adff72697bc4421bbb54856880364d456
Renato Albuquerque <renatomalb83@gmail.com>
2023-09-25 00:03:33 -0300

    valid apiKey and run the data

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,15 @@
+/* 
+    sources to valid api key:
+    https://openweathermap.org/appid#example
+    https://home.openweathermap.org/api_keys
+*/
 
+const apiKey = "a0b6dbf50b04399ef8f5079a298d1e04"
+const cityName = "florianopolis"
 
+const url = `https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${apiKey}&q=${cityName}&units=standard`;
 
-const url = 'https://openweather43.p.rapidapi.com/forecast?appid=da0f9c8d90bde7e619c3ec47766a42f4&q=fortaleza&units=standard';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'b562d0e6aemsh3399129b8d94f0bp196ddejsna5c04166f93a',
-		'X-RapidAPI-Host': 'openweather43.p.rapidapi.com'
-	}
-};
-
-async function weather() {
+async function weatherForecast() {
 try {
 	const response = await fetch(url);
     const data = await response.json();
@@ -20,6 +19,4 @@ try {
 }
 }
 
-weather()
-
-
+weatherForecast()
