0d744d6fd57ee20d7cc9450e1f8d5e30c6b494b2
Renato Albuquerque <renatomalb83@gmail.com>
2023-09-26 23:39:04 -0300

    js test, function weatherForecast

--- js/scripts.js
+++ js/scripts.js
@@ -11,12 +11,34 @@
     Source: https://openweathermap.org/faq#:~:text=Temperature%20is%20available%20in%20Fahrenheit,units%20parameter%20in%20API%20calls.
 */
 
+// variables
+
+let date = document.querySelector(".date span")
+let weather1 = document.querySelector(".weather-1")
+let weather2 = document.querySelector(".weather-2")
+let weather3 = document.querySelector(".weather-3")
+let weather4 = document.querySelector(".weather-4") 
+let button = document.querySelector(".button") 
+
+let todayDate = new Date();
+console.log(todayDate);
+date.textContent = `${todayDate.getDate()}/${todayDate.getMonth()+1}/${todayDate.getFullYear()}`;
+
 const apiKey = "a0b6dbf50b04399ef8f5079a298d1e04"
 const cityName = "florianopolis"
 
 const url = `https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${apiKey}&q=${cityName}&units=metric`;
 
-async function weatherForecast() {
+
+// events
+
+button.addEventListener("click", weatherForecastOne)
+
+
+// functions
+
+async function weatherForecastOne() {
+
 try {
 	const response = await fetch(url);
     const data = await response.json();
@@ -24,7 +46,10 @@ try {
 } catch (error) {
 	console.error(error);
 }
+
+weather1.textContent = "weather 01";
+weather1.textContent = data;
+weather2.textContent = "weather 02";
 }
 
-weatherForecast()
 
